1a5750dd79bd747312c4f49b03594b7f65cf32a6
Kaden <k.j.beckstead@gmail.com>
2019-03-09 10:51:58 -0700

    started info section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,15 @@
 <template>
   <!-- <div>HOME!</div> -->
   <md-card class="card">
-    <div class="poly"></div>
-    <img class="photo" src="../assets/steps.jpg">
+    <div class="info">
+      <div class="content">
+        <h2>Hi, I'm Kaden</h2>
+        <h5>Software Engineer & Student</h5>
+        <md-divider class="divider"></md-divider>
+        <md-icon>email</md-icon>
+      </div>
+    </div>
+    <img class="photo" src="../assets/temple.jpg">
   </md-card>
 </template>
 
@@ -23,19 +30,35 @@ export default class Home extends Vue {}
   height: 65vh;
   min-height: 300px;
   width: auto;
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 40% 100%);
 }
 .card {
   display: flex;
   flex-direction: row;
-  background-color: rgb(165, 165, 165);
+  background-color: rgb(178, 190, 255);
   height: 65vh;
   min-height: 300px;
 }
-.poly {
+
+.divider {
+  width: 80%;
+  background-color: black;
+  margin-bottom: 30px;
+}
+
+.content {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  width: 45%;
+}
+.info {
+  display: flex;
+  flex-direction: row;
   height: 65vh;
   width: 80%;
-  background-color: green;
-  -webkit-clip-path: polygon(0 0, 64% 0, 100% 100%, 0% 100%);
-  clip-path: polygon(0 0, 75% 0, 100% 100%, 0% 100%);
+  /* background-color: green; */
+  /* -webkit-clip-path: polygon(0 0, 64% 0, 100% 100%, 0% 100%);
+  clip-path: polygon(0 0, 75% 0, 100% 100%, 0% 100%); */
 }
 </style>
